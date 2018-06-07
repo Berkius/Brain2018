887458d1a6c042ccec0f984a8a864b39961cba6c
fridakarin <friolo@student.chalmers.se>
2018-06-07 13:46:34 +0200

    Updated

--- serialcomTest/serialcomTest.ino
+++ serialcomTest/serialcomTest.ino
@@ -7,37 +7,40 @@ boolean stringComplete = false;   // used to determine when the string is comple
 
 void setup() {
   // put your setup code here, to run once:
-    // initialize serial
+  // initialize serial
   Serial.begin(9600);             // opens serial port, sets data rate to 9600 bps
-  // HÄÄÄÄÄÄÄÄÄÄÄÄR NEDAN ÄNDRAT
-  mySerial.begin(9600);
-  //mySerial.begin(9600); //HÅLL KOLL
   inputString.reserve(200);       // reserve 200 bytes for the inputString
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  serialEvent();
+  serialEvent();      // Creates the array of information from
 
-  // prints the string from the serial port
-  //Serial.println("-");
-  //Serial.print("String = " + inputString + "\r\n"); // prints the string, BUT WHAT IS THIS LAST PART??
+  showNewData();      // Runs the show data function
 }
 
 
-const byte numChars = 20;
-char receivedChars[numChars];
+
+
+-------------------------------------------------------------------------------------------------
+SerialEvent occurs whenever a new data comes in the hardware serial RX. 
+This routine is run between eash time loop() runs, so using delay inside 
+loop can delay response. Multiple bytes of data may be available. 
+--------------------------------------------------------------------------------------------
+
 
 void serialEvent() {
-    static byte ndx = 0;
+    const byte numChars = 20;           // just to have a maximum of chars
+    char receivedChars[numChars];       // array of chars to store the values in
+    static byte ndx = 0;                
     char endMarker = '\n';
-    char rc;
+    char inChar;                        // the char that is coming in
     while (Serial.available() > 0 && stringComplete == false) {
-        rc = Serial.read();
+        inChar = Serial.read();
 
-        if (rc != endMarker) {
-            receivedChars[ndx] = rc;
+        if (inChar != endMarker) {
+            receivedChars[ndx] = inChar;
             ndx++;
             if (ndx >= numChars) {
                 ndx = numChars - 1;
@@ -50,28 +53,40 @@ void serialEvent() {
         }
     }
 }
+
+void showNewData() {
+ if (stringComplete == true) {
+ Serial.print("This just in ... ");
+ Serial.println(receivedChars);
+ stringComplete = false;
+ }
+}
+
 /*
+ * 
+ */
+
+ /*
 void serialEvent() {
-/*
+
 -------------------------------------------------------------------------------------------------
 SerialEvent occurs whenever a new data comes in the hardware serial RX. 
 This routine is run between eash time loop() runs, so using delay inside 
 loop can delay response. Multiple bytes of data may be available. 
 --------------------------------------------------------------------------------------------
 
-// HÄR MED
-  Serial.println("test1");
-  mySerial.listen();
-  Serial.println("test2");
-  while (mySerial.available() > 0)  {         // checks if there are any information available in the serial port
-  // INTE HÄR DVS DEN FÅR INGEN DATA 
-  Serial.println("test3");
-  char inChar = mySerial.read();  // declares a char and put the information from the serial port in it. DON`T KNOW WHAT (char) DOES??
-  inputString += inChar;              // add the new char to the input string
+  char endMarker = '\n';
+  Serial.listen();
+  
+  while (Serial.available() > 0)  {         // checks if there are any information available in the serial port
+    Serial.println("test3");
+    char inChar = Serial.read();  // declares a char and put the information from the serial port in it. DON`T KNOW WHAT (char) DOES??
+    inputString += inChar;              // add the new char to the input string
 
-  if(inChar == '{\r')  {              // if the incoming character is a newline, set a flag -> true so that the loop can do something = extract datas. WHAT IS '/r'???
-    stringComplete = true; 
-  }
+          if (inChar == endMarker) {
+                  stringComplete = true;
+              }
+          
   
  }
-}*/
+} */
