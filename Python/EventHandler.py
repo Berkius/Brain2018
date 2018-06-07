ced8437dfecab22893cd5b2511bb0c96b4cd3c98
unknown <berkius@student.chalmers.se>
2018-06-07 14:03:15 +0200

    event handler

---
+++ Python/EventHandler.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class MyHandler(FileSystemEventHandler):
+	#patterns = ["*.raw16", "*.bin"]
+
+	def process(self, event):
+	    """
+	    event.event_type 
+	        'modified' | 'created' | 'moved' | 'deleted'
+	    event.is_directory
+	        True | False
+	    event.src_path
+	        path/to/observed/file
+	    """
+	    # the file will be processed there
+	    print(event.src_path, event.event_type)  # print now only for degug
+
+	def on_modified(self, event):
+	    self.process(event)
+
+	def on_created(self, event):
+	    self.process(event)
+
+	def on_any_event(self,event):
+		self.process(event)    
+
+if __name__ == '__main__':
+	args = sys.argv[1:] 											#take argument as the second line in the system input
+	observer = Observer()											#create an observer object
+	observer.schedule(MyHandler(), path=args[0], recursive=False)	#take the first line in the argument as a path
+	observer.start()												#Start observing that folder
+
+	try:
+	    while True:
+	        time.sleep(1)
+	except KeyboardInterrupt:
+	    observer.stop()
+
+observer.join()
---
+++ Python/ReadFiles.py
@@ -0,0 +1,43 @@
+
+import os 
+
+i=0
+
+file = open("C:\\Users\\Rickard\\Desktop\\Brains2018\\PassList.txt", "r") 
+lines=file.readlines()
+
+for i in range(0,len(lines)):
+
+	#look for file in folder
+	#Maybe write code to skip line to when overlapping
+		
+		if 'FENG' in lines[i]:
+			print(lines[i])
+			#do function for proccessing
+
+		if 'METOP' in lines[i]:
+			print(lines[i])
+			#do function for proccessing
+		if 'METEOR' in lines[i]:
+			print('RUS??? Do not want')
+			#skipp?
+		if 'NOAA' in lines[i]:
+			print('Go go go NOAA')
+			#call function for HRPT_Reader	
+
+
+
+#for line in file:
+#	i=i+1
+#	if (i==8):
+#		print(line)
+#		if(line[4]=='F'):
+#			print("success")
+#			#Put function for Fengyun here  
+#		if(line[7]=='O'):
+#			print('success MET')	
+#			#put funtion for Metop her 
+#		else: 
+#			print("sorry")
+
+
