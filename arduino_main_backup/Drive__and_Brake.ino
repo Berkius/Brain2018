189abbdcdd49af88880184ad1bf2b29284c2e3d2
unknown <goitia@student.chalmers.se>
2018-06-18 10:46:04 +0200

    motor controller backup

---
+++ arduino_main_backup/Drive__and_Brake.ino
@@ -0,0 +1,47 @@
+/*
+ * All the functions that tells the motor how to run
+ * 
+ * Date: 2018-06-16
+ */
+
+// ROLL
+// Create a positive roll motion (lower motor)
+void Roll_Positive(int Speed) {
+     digitalWrite(roll_IN1,HIGH); 
+     digitalWrite(roll_IN2,LOW);  
+     analogWrite(roll_PWM,Speed);
+}
+
+// Create a negative roll motion (lower motor)
+void Roll_Negative(int Speed) {    
+     digitalWrite(roll_IN1,LOW); 
+     digitalWrite(roll_IN2,HIGH);  
+     analogWrite(roll_PWM,Speed);
+}
+
+// Breake the roll motion (lower motor)
+void Roll_Brake(){
+     digitalWrite(roll_IN1,LOW); 
+     digitalWrite(roll_IN2,LOW); 
+}
+
+// PITCH
+// Create a positive pitch motion (upper motor)
+void Pitch_Positive(int Speed) {
+     digitalWrite(pitch_IN1,HIGH); 
+     digitalWrite(pitch_IN2,LOW);  
+     analogWrite(pitch_PWM,Speed);
+}
+
+// Create a negative pitch motion (upper motor)
+void Pitch_Negative(int Speed) {    
+     digitalWrite(pitch_IN1,LOW); 
+     digitalWrite(pitch_IN2,HIGH);  
+     analogWrite(pitch_PWM,Speed);
+}
+
+// Breake the pitch motion (upper motor)
+void Pitch_Brake(){
+     digitalWrite(pitch_IN1,LOW); 
+     digitalWrite(pitch_IN2,LOW); 
+}
---
+++ arduino_main_backup/accelerometer_setup.ino
@@ -0,0 +1,25 @@
+/*
+ * Calibrate the accelerometer
+ * 
+ * Date: 2018-06-16
+ */
+
+void accelerometer_setup(){
+ acc.powerOn();
+ 
+ for (int i = 0; i <= 200; i++) {
+  acc.readAccel(&ax, &ay, &az);
+  
+  if (i == 0) {
+    aoffsetX = ax;
+    aoffsetY = ay;
+    aoffsetZ = az;
+  }
+  if (i > 1) {
+    aoffsetX = (ax + aoffsetX) / 2;
+    aoffsetY = (ay + aoffsetY) / 2;
+    aoffsetZ = (az + aoffsetZ) / 2;
+  }
+ }
+}
+
