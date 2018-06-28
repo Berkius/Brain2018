8a44ad5fb5f7d93814448e805aafe8b73e29a714
unknown <berkius@student.chalmers.se>
2018-06-28 16:24:05 +0200

    calibration for azimuth

--- arduino_main/Calibration_tab.ino
+++ arduino_main/Calibration_tab.ino
@@ -43,7 +43,6 @@ void Calibration(){
       Serial.println("Switch unactivated");
 
       /*
-       * 
        * FOR EVELEVATION SWITCH NO 2 (WE PROB DEONT NEED THIS)
       while(digitalRead(sensor_el)==LOW){           //When low again dirve backwards  
         Pitch_Negative(setupSpeed);                 //Drive Backwards 
@@ -62,7 +61,7 @@ void Calibration(){
         // WE WANT THIS DEPENDENT ON OFFSET?
         elevation_center=abs((elevation_min+elevation_max)/2);   //Get centered coordinate
         
-        //THE CALIBRATION IS NOW DONE AND THE NEXT WHILE LOOP WILL JUST PLACE THE ANTENNA IN THE CENTER.
+        //THE CALIBRATION FOR THE ELEVATION IS NOW DONE AND THE NEXT WHILE LOOP WILL JUST PLACE THE ANTENNA IN THE CENTER.
       Serial.println("Centering the parabola");
       /*while(!((pitchdeg-offset_el)<=(elevation_center+2) && (pitchdeg-offset_el)>=(elevation_center-2))){         //As long as the angle is out of the interval keep on going
           getCurrentAngles();
@@ -72,48 +71,36 @@ void Calibration(){
       Pitch_Brake();*/
       Serial.println("Centering finished");
       
-      /*  
+      Serial.println("Starting the Calibration for Azimuth angle")  
       //FOR THE LOWER MOTOR CONTROLLING THE AZIMUTH
-     
-      while (sensorValue_2==LOW){
-        Roll_Negative(setupSpeed);
-        delay(DelayVar);
-        getCurrentAngles();
-        delay(1000);
-        Serial.println("First While");
+      Serial.println("Waiting for the switch to be pushed");
+       while (digitalRead(sensor_az)==LOW){
+        Roll_Positive(setupSpeed);
+        delay_s(DelayVar);                                //Was needed to know for how long to run, delay_s makes it possible to brake the drive in the middle of a command. 
+        getCurrentAngles();                               //Gets the angles from accelerometer 
         }
-      while (sensorValue_1==HIGH){
-        Roll_Brake();
-        getCurrentAngles();              //Get current angle from the accelerometer
-        delay(1000);
-        if (rolldeg!=0){
-          offset_az=rolldeg;
-          }     
-        azimuth_min=rolldeg-offset_az;                 //minimum degree for the parabola
-        //Save the acc.meters value
-        Roll_Positive(setupSpeed);
-        delay(DelayVar);
-        Serial.println("second while");
-        Serial.println("Print Offset");
-        Serial.println(offset_az);
+        Roll_Brake();                                      //When Sensor_el turns to High, brake
+        Serial.println("Switch activated");                   
+  
+        // Log offset value                                          
+        getCurrentAngles();                               //Get current angle from the accelerometer  
+        if (azimuth_min!=rolldeg){                     //If they are not equal save the offset 
+            offset_az=abs(azimuth_min-rolldeg);        //Set the offset from zero, the offset_az needs to be used when checking the degree further in the program
+            }   
+        Serial.print("offset_az: ");                //for testing purpose 
+        Serial.println(offset_az);                  //for testing porpuse
+  
+        // Drive elevtation negative until switch is unactivated
+        Serial.println("Drive azimuthal motor negative until switch is unactivated..");
+        while(digitalRead(sensor_az)==HIGH){  
+  
+          Roll_Negative(setupSpeed);                  //Drive Backwards
+          delay_s(DelayVar);
+          //Serial.println("inside high loop");
         }
-      while(sensorValue_2==LOW){
-        Roll_Positive(setupSpeed);
-        delay(DelayVar);
-        getCurrentAngles();
-        delay(1000);
-        Serial.println("third while");
-        } 
-      while(sensorValue_2==HIGH){
-        Roll_Brake();
-        getCurrentAngles();            //Get current angle from the accelerometer
-        delay(1000);
-        azimuth_max=rolldeg-offset_az;
-        Roll_Positive(setupSpeed);
-        delay(DelayVar);
-        Serial.println("Last while");
-        } 
-        /*azimuth_center=(azimuth_min+azimuth_max)/2;   //Reletiv centered coordinate
+        Serial.println("Switch unactivated");
+
+      /*azimuth_center=(azimuth_min+azimuth_max)/2;   //Reletiv centered coordinate
       while(!(rolldeg<=(azimuth_center+2) && rolldeg>=(azimuth_center-2))){         //As long as the angle is out of the interval keep on going
           getCurrentAngles();
           Roll_Positive(setupSpeed);
