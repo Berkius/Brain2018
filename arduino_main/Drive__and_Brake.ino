16f0e34c3bfe3efd1377474232b97e6d42f26b9c
AnnieEdvardsson <37796778+AnnieEdvardsson@users.noreply.github.com>
2018-06-29 13:47:58 +0000

    Add files via upload

---
+++ arduino_main/Drive__and_Brake.ino
@@ -0,0 +1,89 @@
+/*
+ * All the functions that tells the motor how to run
+ * 
+ * Date: 2018-06-16
+ */
+
+// ROLL
+// Create a positive roll motion (lower motor)
+void Roll_Positive(int Speed) {
+  int safe_marg = 5;
+  if (rolldeg < 540 - safe_marg){
+     //Serial.println("Entering Roll_Positive");
+     motor_direction=3;  //drive forward roll
+     digitalWrite(roll_IN1,HIGH); 
+     digitalWrite(roll_IN2,LOW);  
+     analogWrite(roll_PWM,Speed);
+     //Serial.println("Ending Roll_Positive");
+  }
+  else{
+    //Serial.println(F("Roll reached maximum value"));
+    }
+  }
+
+// Create a negative roll motion (lower motor)
+void Roll_Negative(int Speed) {    
+  int safe_marg = 5;
+  if (rolldeg >= -540 + safe_marg){
+     //Serial.println("Entering Roll_Negative");
+     motor_direction=4;  //drive backwards roll
+     digitalWrite(roll_IN1,LOW); 
+     digitalWrite(roll_IN2,HIGH);  
+     analogWrite(roll_PWM,Speed);
+     //Serial.println("Ending Roll_Negative");
+  }
+  else{
+    //Serial.println(F("Roll reached minimum value"));
+  }
+ }
+
+// Breake the roll motion (lower motor)
+void Roll_Brake(){
+     //Serial.println("Entering Roll_Brake");
+     digitalWrite(roll_IN1,LOW); 
+     digitalWrite(roll_IN2,HIGH); 
+     //Serial.println("Ending Roll_Brake");
+}
+
+// PITCH
+// Create a positive pitch motion (upper motor)
+void Pitch_Positive(int Speed) {
+  int safe_marg = 5;
+  if (pitchdeg < elevation_max - safe_marg){
+     //Serial.println("Entering Pitch_Positive");
+     motor_direction=1;  //drive forward pitch
+     digitalWrite(pitch_IN1,HIGH); 
+     digitalWrite(pitch_IN2,LOW);  
+     analogWrite(pitch_PWM,Speed);
+     //Serial.println("Ending Pitch_Positive");
+  }
+  else{
+    //Serial.println(F("Pitch reached maximum value"));
+    }
+}
+
+// Create a negative pitch motion (upper motor)
+void Pitch_Negative(int Speed) {  
+  int safe_marg = 5;
+  if (pitchdeg >= elevation_min + safe_marg){
+     //Serial.println("Entering Pitch_Negative");
+     motor_direction=2; //drive backwards pitch
+     digitalWrite(pitch_IN1,LOW); 
+     digitalWrite(pitch_IN2,HIGH);  
+     analogWrite(pitch_PWM,Speed);
+     //Serial.println("Ending Pitch_Negative");
+  }
+  else{
+  //Serial.println(F("Pitch reached minimum value"));
+  }
+ }
+
+
+// Breake the pitch motion (upper motor)
+void Pitch_Brake(){
+     //Serial.println("Entering Pitch_Brake");
+     digitalWrite(pitch_IN1,LOW); 
+     digitalWrite(pitch_IN2,LOW); 
+     //Serial.println("Ending Pitch_Brake");
+}
+
---
+++ arduino_main/getCurrentPitch_tab.ino
@@ -0,0 +1,30 @@
+void getCurrentPitch(){
+/*
+ * Calculate the pitch and roll angle with sensor data from the accelerometer
+ * 
+ * Date: 2018-06-16
+ */
+ //Serial.println("Entering getCurrentPitch");
+ float X, Y, Z;
+ int rawX, rawY, rawZ;
+
+// code fragment for Accelerometer angles (roll and pitch)
+  acc.readAccel(&ax, &ay, &az); //read the accelerometer values and store them in variables  x,y,z
+  rawX = ax - aoffsetX;
+  rawY = ay - aoffsetY;
+  rawZ = az  - (255 - aoffsetZ);
+  
+  X = rawX/256.00; // used for angle calculations
+  Y = rawY/256.00; // used for angle calculations
+  Z = rawZ/256.00; // used for angle calculations
+  
+  // calculated angle in degrees (OBS in calibration offset will be zero) 
+
+  pitchdeg = 180*(atan(X/sqrt(Y*Y+Z*Z)))/PI + offset_el; 
+ // pitchdeg = PITCH_T+offset_az;
+
+  Serial.print("pitch angle: ");
+  Serial.println(pitchdeg);
+
+  //Serial.println("Ending getCurrentPitch");
+}
